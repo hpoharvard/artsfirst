fcce63be1b259099e1190e388db685653ab752dd
Giovanni Zambotti <g.zambotti@gmail.com>
2017-03-17 16:02:41 -0400

    added popup, rendering style

--- js/artsfirst.js
+++ js/artsfirst.js
@@ -2,6 +2,9 @@ require([
       "esri/Map",
       "esri/views/MapView",
       "esri/layers/FeatureLayer",
+      "esri/renderers/SimpleRenderer",
+      "esri/symbols/SimpleMarkerSymbol",
+      "esri/symbols/SimpleFillSymbol",
       "esri/renderers/UniqueValueRenderer",
       "esri/widgets/Search",
       "esri/widgets/Popup",
@@ -15,10 +18,77 @@ require([
       "calcite-maps/calcitemaps-v0.3",
 
       "dojo/domReady!"
-    ], function(Map, MapView, FeatureLayer, UniqueValueRenderer, Search, Popup, query) {
+    ], function(Map, MapView, FeatureLayer, SimpleRenderer, SimpleMarkerSymbol, SimpleFillSymbol, UniqueValueRenderer, Search, Popup, query) {
 
       var artsLocationUrl = "https://hppm-dev.cadm.harvard.edu/arcgis/rest/services/ArtsFirst/ArtsFirst/MapServer/2";
-   
+      
+      /*var aRenderer = new SimpleRenderer({
+        symbol: new SimpleMarkerSymbol({
+          size: 8,
+          color: "black",
+          outline: {
+            width: 1,
+            color: "red"
+          }
+        })
+      });*/
+
+      var symConcert = new SimpleMarkerSymbol({        
+        size: 8,
+        color: "#a6cee3",
+        style:"circle",
+        outline: {
+          width: 0.5,
+          color: "black"
+        }
+      });
+
+      var symExhibition = new SimpleMarkerSymbol({
+        size: 8,
+        color: "#1f78b4",
+        style:"circle",
+        outline: {
+          width: 0.5,
+          color: "black"
+        }
+      });
+
+      var symInstallation = new SimpleMarkerSymbol({
+        size: 8,
+        color: "#b2df8a",
+        style:"circle",
+        outline: {
+          width: 0.5,
+          color: "black"
+        }
+      });
+
+      var aRenderer = new UniqueValueRenderer({
+        field: "Venue_Type",
+        //defaultSymbol: new SimpleFillSymbol()
+        uniqueValueInfos: [
+          {
+            value: "Concert",  // value "Concert"
+            symbol: symConcert  // will be assigned symConcert
+          }, {
+            value: "Exhibition",  // value "Exhibition"
+            symbol: symExhibition  // will be assigned symExbition
+          }, 
+          {
+            value: "Installation",  // value "Exhibition"
+            symbol: symInstallation  // will be assigned symExbition
+          },          
+        ]
+      });
+
+      // Create the PopupTemplate
+        var popupTemplate = {
+          title: "{Name}",
+          content: "<p><b> Address: {Address} </b></p>" +
+            "<p> Room: {Room}</p>" +
+            "<p> Zone: {Zone}</p>" 
+        };      
+      
       /******************************************************************
        *
        * Create the map, view and widgets
@@ -27,8 +97,9 @@ require([
       var artsLayer = new FeatureLayer({
         url: artsLocationUrl,
         outFields: ["*"],
+        popupTemplate: popupTemplate,
         visible: true,
-        //renderer: buildingRenderer
+        renderer: aRenderer
       });
       // Map
       var map = new Map({
